29e8c5586e4267ba5186c8fa99aca730519ac89a
RincewindWizzard <git@magierdinge.de>
2023-12-09 15:59:28 +0100

    pint

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,8 @@ mod minuterie;
 
 
 fn main() {
-    let launch = Instant::now();
-    let mut minuterie = Minuterie::new(Duration::from_secs(1));
-    let (tx, rx) = minuterie.start();
-
+    // let launch = Instant::now();
+    // let mut minuterie = Minuterie::new(Duration::from_secs(1));
+    // let (tx, rx) = minuterie.start();
 }
 
--- src/minuterie.rs
+++ src/minuterie.rs
@@ -1,17 +1,21 @@
+use std::fmt::{Display, Formatter};
 use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, Sender};
+use std::sync::mpsc::{Receiver, Sender, SendError};
 use std::thread;
 use std::thread::JoinHandle;
 use std::time::{Duration, Instant};
+use crate::minuterie::State::{ACTIVE, INACTIVE};
+
+const MINUTERIE: &str = "Minuterie";
 
 pub struct Event {
     pub instant: Instant,
     pub topic: String,
-    pub content: String,
+    pub content: State,
 }
 
 impl Event {
-    pub fn from(topic: String, content: String) -> Event {
+    pub fn from(topic: String, content: State) -> Event {
         Event {
             instant: Instant::now(),
             topic,
@@ -20,39 +24,69 @@ impl Event {
     }
 }
 
+#[derive(Debug)]
+enum State {
+    ACTIVE,
+    INACTIVE,
+}
+
 pub struct Minuterie {
     last_event: Instant,
     timeout: Duration,
+    rx: Receiver<Event>,
+    tx: Sender<Event>,
 }
 
+
+trait Publisher {
+    fn publish(&self, state: State) -> Result<(), SendError<Event>>;
+}
+
+impl Publisher for Sender<Event> {
+    fn publish(&self, state: State) -> Result<(), SendError<Event>> {
+        self.send(Event::from(MINUTERIE.to_string(), state))
+    }
+}
+
+
 impl Minuterie {
-    pub fn new(timeout: Duration) -> Minuterie {
-        // create a minuterie with elapsed timeout so it is not currently active
-        Minuterie {
-            last_event: Instant::now() - timeout,
-            timeout
-        }
-    }
-
-    fn run(&self, rx: Receiver<Event>, tx: Sender<Event>) {
-        let timeout = &self.timeout;
-        while let Ok(event) = rx.recv() {
-            if let Err(error) = tx.send(event) {
-                println!("Error {}", error);
-                break;
-            }
-        }
-    }
-
-    pub fn start(self) -> (Sender<Event>, Receiver<Event>) {
+    pub fn start(timeout: Duration) -> (Sender<Event>, Receiver<Event>) {
         let (input_tx, input_rx) = mpsc::channel();
         let (output_tx, output_rx) = mpsc::channel();
+
+        // create a minuterie with elapsed timeout so it is not currently active
+        let mut minuterie = Minuterie {
+            last_event: Instant::now() - timeout,
+            timeout,
+            rx: input_rx,
+            tx: output_tx,
+        };
+
         thread::spawn(move || {
-            self.run(input_rx, output_tx)
+            minuterie.run()
         });
         (input_tx, output_rx)
     }
 
+    fn current_state(&self) -> State {
+        if Instant::now().duration_since(self.last_event) < self.timeout {
+            ACTIVE
+        } else {
+            INACTIVE
+        }
+    }
+
+    fn run(&mut self) -> Result<(), SendError<Event>> {
+        let timeout = &self.timeout;
+
+        self.tx.publish(self.current_state())?;
+
+        while let Ok(event) = self.rx.recv() {
+            self.last_event = event.instant;
+            self.tx.publish(self.current_state())?;
+        }
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -61,24 +95,24 @@ mod tests {
     use std::time::{Duration, Instant};
     use rand::Rng;
     use crate::minuterie::{Event, Minuterie};
+    use crate::minuterie::State::ACTIVE;
 
     #[test]
     fn minuterie_timing() {
         let launch = Instant::now();
-        let mut minuterie = Minuterie::new(Duration::from_secs(1));
-        let (tx, rx) = minuterie.start();
+        let (tx, rx) = Minuterie::start(Duration::from_secs(1));
 
         thread::spawn(move || {
             for i in 1..10 {
                 let mut rng = rand::thread_rng();
-                thread::sleep(Duration::from_millis(rng.gen_range(0..1000)));
-                tx.send(Event::from(format!("Test-{i}"), format!("alive"))).unwrap();
+                thread::sleep(Duration::from_millis(rng.gen_range(0..100)));
+                tx.send(Event::from(format!("Test-{i}"), ACTIVE)).unwrap();
             }
         });
 
         while let Ok(event) = rx.recv() {
             println!(
-                "Event({}, {}, {})",
+                "Event({}, {}, {:?})",
                 event.instant.duration_since(launch).as_millis(),
                 event.topic,
                 event.content
